bf3562aa731b13d1553e484207d253810db6bbe4
senkyojapan <senkyo.japan@gmail.com>
2026-07-20 15:09:42 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { DashboardProvider } from './context/DashboardContext'
 import DashboardPage from './pages/DashboardPage'
 import ChurchDetailPage from './pages/ChurchDetailPage'
 import NotFoundPage from './pages/NotFoundPage'
+import PasswordGate from './components/PasswordGate'
 
 export default function App() {
   return (
-    <DashboardProvider>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/church/:id" element={<ChurchDetailPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </HashRouter>
-    </DashboardProvider>
+    <PasswordGate>
+      <DashboardProvider>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/church/:id" element={<ChurchDetailPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </HashRouter>
+      </DashboardProvider>
+    </PasswordGate>
   )
 }
